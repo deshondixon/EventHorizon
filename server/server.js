5819f3f467fc3fcc0a3a499847362070bbe6d127
DeShon Dixon <deshondixon@gmail.com>
2024-06-07 12:39:56 -0500

    server changes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ const routes = require('./controllers/api');
 
 const app = express();
 
+app.use(
+  cors({
+    origin: 'https://heroku-event-horizon-app-b15e4591d5a2.herokuapp.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Authorization', 'Content-Type'],
+  })
+);
+
+app.use(routes);
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('/*', (req, res) => {
@@ -13,11 +23,14 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
 
-app.use(routes);
+// app.use(routes);
 
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
