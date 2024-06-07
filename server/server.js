54945dfc05f96ad50ad95f5acffe0b66a4f6ec2e
DeShon Dixon <deshondixon@gmail.com>
2024-06-07 15:33:50 -0500

    main

--- server/server.js
+++ server/server.js
@@ -32,8 +32,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// app.use(routes);
-
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
 });
